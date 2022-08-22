6d314fc4e09aca3981c689163e99d261db3f85e3
aborgessouzameli <alexandre.bsouza@mercadolivre.com>
2022-08-22 17:45:31 -0300

    chore: removed packages not used

--- structural/AdapterPattern/Adapter/src/br/com/meli/solid/structural/adapter/Main.java
+++ structural/AdapterPattern/Adapter/src/br/com/meli/solid/structural/adapter/Main.java
@@ -1,9 +1,7 @@
 package br.com.meli.solid.structural.adapter;
 
 import br.com.meli.solid.structural.adapter.adapter.MercadoPagoAdapter;
-import br.com.meli.solid.structural.adapter.adapter.PayonnerAdapter;
 import br.com.meli.solid.structural.adapter.gateway.mercadopago.MercadoPagoPayment;
-import br.com.meli.solid.structural.adapter.gateway.payoneer.Payonner;
 import br.com.meli.solid.structural.adapter.gateway.paypal.IPayPalPayments;
 
 public class Main {
